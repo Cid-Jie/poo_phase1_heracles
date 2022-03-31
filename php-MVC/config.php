f48ae9b2daa6b5f6b74c08ee0fd00a80c8e6c10d
Cid-Jie <pcidjie@gmail.com>
2022-03-31 15:52:28 +0200

    remove file

--- php-MVC/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('USER', 'CidJie');
-define('PASSWORD', 'Chouchou123!');
-define('DATABASE', 'marmiwild');
-define('SERVER', 'localhost');
--- php-MVC/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Va gérer toutes les requêtes vers mes pages web
-//controller frontal
-
-require __DIR__.'/../config.php';
-require __DIR__.'/../src/routing.php';
-
-
-
