0eb31959a8781b733f22b41cac9eda7908fc133a
Cidjie Prefol <76404051+Cid-Jie@users.noreply.github.com>
2022-04-28 08:53:48 +0200

    Update index.php
    remove commentary

--- public/index.php
+++ public/index.php
@@ -11,30 +11,6 @@ $lion = new Fighter('🦁Lion de Néméé', 20, 13);
 var_dump($heracles);
 var_dump($lion);
 
-
-while($heracles->getLife() !=0 && $lion->getLife() !=0)
-{
-    $damage = $heracles->fight($lion);
-    echo $heracles->getName() . " attaque " . $lion->getName() . " et lui fais perdre $damage points de vie. </br>";
-    echo "Point de vie : " .$lion->getName() ." ". $lion->getLife() ."</br>";
-        
-            if($lion->getLife() == 0)
-            break;
-
-    $damage = $lion->fight($heracles);
-    echo $lion->getName() . " attaque " . $heracles->getName() . " et lui fais perdre $damage points de vie. </br>";
-    echo "Point de vie : " .$heracles->getName() ." ". $heracles->getLife()."</br>";
-}
-
-if($heracles->getLife() == 0)
-{
-    echo $lion->getname() . " a gagné!";
-}else{
-    echo $heracles->getName() . " a gagné!";
-}
-
-
-//////Correction avec la fonction isAlive
 while($heracles->isAlive() && $lion->isAlive())
 {
     $damage = $heracles->fight($lion);
@@ -57,10 +33,3 @@ if($lion->isAlive())
 }
 
 
-/*while($fighter->life > 0){
-    $round = 
-    $fighter1->fight($fighter2);
-    $fighter2->fight($fighter1);
-    $round ++;
-
-}*/
